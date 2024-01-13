cb25d5fc748863c2528c0967b0c78edfca87837f
florina-alfred <sonofcop@gmail.com>
2024-01-13 05:21:57 +0100

    first commit

---
+++ after/plugin/conform.lua
@@ -0,0 +1,12 @@
+require("conform").setup({
+    formatters_by_ft = {
+        python = { "black" },
+        go = { "gofmt" },
+        javascript = { { "prettierd", "prettier" } },
+    },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
+})
