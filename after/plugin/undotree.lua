5c417ea6e3daa2304a477a7b5c80a621a929b788
Florina-Alfred <sonofcop@gmail.com>
2025-06-19 22:41:54 +0200

    added focus on undotree call

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,2 +1,4 @@
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+-- Set undotree to focus the undotree panel when toggled
+vim.g.undotree_SetFocusWhenToggle = 1
 
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
