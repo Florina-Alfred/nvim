f63b354c594233412b66b5f760eecc20c1a40d4f
Florina-Alfred <sonofcop@gmail.com>
2025-07-30 16:19:06 +0200

    finalized auto_suggestions

--- after/plugin/avante.lua
+++ after/plugin/avante.lua
@@ -10,7 +10,7 @@ require("avante").setup({
     -- add any opts here
     -- for example
     system_prompt = read_system_prompt(),
-    -- auto_suggestions_provider = "copilot",
+    auto_suggestions_provider = "copilot",
     provider = "copilot",
     providers = {
         copilot = {
@@ -26,8 +26,8 @@ require("avante").setup({
     --     },
     -- },
     behaviour = {
-        -- auto_suggestions = true, -- Experimental stage
-        auto_suggestions = false, -- Experimental stage
+        auto_suggestions = true, -- Experimental stage
+        -- auto_suggestions = false, -- Experimental stage
         auto_set_highlight_group = true,
         auto_set_keymaps = true,
         auto_apply_diff_after_generation = false,
