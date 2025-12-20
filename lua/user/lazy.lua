7db30b2391b98c93bddca8e6b0ff5e9229e0702f
Florina-Alfred <sonofcop@gmail.com>
2025-12-20 10:55:33 +0100

    added tree-sitter-object

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -185,6 +185,27 @@ require("lazy").setup(
         },
 
         {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            after = "nvim-treesitter",
+            requires = "nvim-treesitter/nvim-treesitter",
+        },
+
+        {
+            "folke/flash.nvim",
+            event = "VeryLazy",
+            ---@type Flash.Config
+            opts = {},
+            keys = {
+                { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+                { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+                { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+                { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+                { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+            },
+        },
+
+        -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+        {
             "yetone/avante.nvim",
             build = function()
                 if vim.fn.has("win32") == 1 then
